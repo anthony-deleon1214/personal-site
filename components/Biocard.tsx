29cf64c27188cae5fda52e6c51fcd83cb544e673
Anthony Deleon <anthony.deleon1214@gmail.com>
2022-09-09 20:17:39 -0400

    Added Biocard component

---
+++ components/Biocard.tsx
@@ -0,0 +1,9 @@
+const Biocard = () => {
+    return (
+        <div className="biocard">
+            <h2>Hello There</h2>
+        </div>
+    )
+};
+
+export default Biocard;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,19 +2,16 @@ import React, { ReactNode } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
 import Sidebar from './Sidebar'
+import Biocard from './Biocard';
 
-type Props = {
-  children?: ReactNode
-  title?: string
-}
-
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = () => (
   <div>
     <Head>
       <title>Anthony Deleon</title>
     </Head>
     <Sidebar></Sidebar>
+    <Biocard></Biocard>
   </div>
 )
 
-export default Layout
+export default Layout;
