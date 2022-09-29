cf56a089426b8f6a263187af94181b88f6046c6e
Anthony Deleon <anthony.deleon1214@gmail.com>
2022-09-29 12:30:46 -0400

    Working on Sanity implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import { sanityClient, urlFor } from '../sanity';
+import { Post } from '../typings';
 
-const IndexPage = () => (
+interface Props {
+  posts: [Post];
+};
+
+const IndexPage = ({ posts }: Props) => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
@@ -13,3 +19,25 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const getServerSideProps = async() => {
+  const query = `*[_type == "post"] {
+    _id,
+    title,
+    author -> {
+      name,
+      image
+  },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
