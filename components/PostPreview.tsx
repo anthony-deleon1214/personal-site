9cbf6187d509995137ee29d0f44a8219f7a18b29
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-10-01 21:00:49 -0400

    Changing PostPreview CSS

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -9,18 +9,20 @@ interface Props {
 export default function PostPreview(Props) {
     const post = Props.post
     return (
-    <div>
-        <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
-                <img src={urlFor(post.mainImage).url()!} alt="" />
-                <div className="post-preview-info">
-                    <p>{post.title}</p>
-                    <p>
-                        {post.description} by {post.author.name}
-                    </p>
+        <div className="post-preview group">
+            <Link key={post._id} href={`/post/${post.slug.current}`}>
+                <div>
+                    <img className="post-preview-img" src={urlFor(post.mainImage).url()!} alt="" />
+                    <div className="post-preview-info">
+                        <div>
+                            <p>{post.title}</p>
+                            <p>
+                                {post.description} by {post.author.name}
+                            </p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </Link>
-    </div>
-);
+            </Link>
+        </div>
+    );
 };
