0078f16598fa5b071c95782bf227c6ca7f60d9d4
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-23 20:38:17 -0500

    Tweaks to resume page CSS

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -2,11 +2,14 @@ import Sidebar from "../components/Sidebar";
 
 function Resume() {
     return (
-        // Placeholder so I don't flashbang myself
-        <div className="flex h-screen w-screen dark:bg-slate-800">
+        // TODO: Implement as a CSS grid
+        <div className="flex h-screen w-screen dark:bg-slate-800 -z-0">
             <Sidebar />
             <div className="header">
                 <span className="nameplate">Anthony Deleon</span>
+            </div>
+            <div className="resume-content">
+
             </div>
         </div>
     )
